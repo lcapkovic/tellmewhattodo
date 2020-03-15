9084cd5fd5f52a0082e445b31f60e8a73dd6ef25
Lukas Capkovic <lcapkovic@gmail.com>
2020-03-14 20:37:47 -0700

    More stuff

--- things_to_do.js
+++ things_to_do.js
@@ -1,10 +1,90 @@
 data = [
     {
-        "name": "Do/Learn/Watch/... this and that",
-        "link": "http://OPTIONAL.LINK"
+        "name": "Call a friend",
+        "link": ""
     },
     {
-        "name": "Thing 2",
+        "name": "Tidy up with Marie Kondo",
+        "link": "https://www.youtube.com/watch?v=TXzcmr2WcDA"
+    },
+    {	
+        "name": "Take a new LinkedIn profile picture",
         "link": ""
-    }
+	},
+     {	
+        "name": "Update your LinkedIn",
+        "link": ""
+	},
+    {	
+        "name": "Write a letter to a friend",
+        "link": ""
+	},
+    {	
+        "name": "Listen to a podcast",
+        "link": ""
+	},
+    {	
+        "name": "Clean your shoes",
+        "link": "https://www.youtube.com/watch?v=kyGR47nDrus"
+	},
+    {	
+        "name": "Make a new song playlist",
+        "link": ""
+	},
+    {	
+        "name": "Rearrange your furniture",
+        "link": ""
+	},
+    {   	
+        "name": "Watch a TED Talk",
+        "link": "https://www.ted.com/recommends?exploreCTASource=topics-directory.banner"
+	},
+    {	
+        "name": "Cook something with what you already have",
+        "link": "https://www.supercook.com/#/recipes"
+	},
+    {	
+        "name": "Clean out your closet and sell old clothes online",
+        "link": "https://www.thredup.com/cleanout"
+	},
+    {	
+        "name": "Take a free online class",
+        "link": "https://learndigital.withgoogle.com/digitalgarage"
+	},
+    {	
+        "name": "Meditate",
+        "link": ""
+	},
+    {	
+        "name": "Download a productive email app",
+        "link": "https://techcommunity.microsoft.com/t5/outlook-blog/five-time-saving-outlook-mobile-tips-for-small-businesses/ba-p/752838"
+	},
+    {	
+        "name": "Learn how to do calligraphy",
+        "link": "https://www.youtube.com/watch?v=sBoVGqiSzr4"
+	},
+    {	
+        "name": "Learn how to take better photos with your phone",
+        "link": "https://www.youtube.com/watch?v=AywNcZL6IQQ"
+	},
+    {	
+        "name": "Create a personal budget ",
+        "link": ""
+	},
+    {	
+        "name": "Go on a walk",
+        "link": ""
+	},
+    {	
+        "name": "Make a new music playlist",
+        "link": ""
+	},
+    {	
+        "name": "Choose and implement a few sustainable practices into your life",
+        "link": "https://theminimalistvegan.com/live-a-more-sustainable-lifestyle/"
+	},
+    {	
+        "name": "Find a joke and tell a friend",
+        "link": "https://www.boredpanda.com/funniest-two-line-jokes/?utm_source=google&utm_medium=organic&utm_campaign=organic"
+	}
 ];
