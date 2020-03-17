6391197f4b8662bdd189b63435e47aa00b319c0d
Matej Capkovic <mcapkovic@gmail.com>
2020-03-17 16:46:33 +0100

    Add new things to do

--- things_to_do.js
+++ things_to_do.js
@@ -1,170 +1,262 @@
 data = [
     {
-        "name": "Call a friend",
-        "link": ""
+        name: 'Call a friend',
+        link: ''
     },
     {
-        "name": "Tidy up with Marie Kondo",
-        "link": "https://www.youtube.com/watch?v=TXzcmr2WcDA"
-    },
-    {	
-        "name": "Take a new LinkedIn profile picture",
-        "link": ""
-	},
-    {	
-        "name": "Update your LinkedIn",
-        "link": ""
-	},
-    {	
-        "name": "Write a letter to a friend",
-        "link": ""
-	},
-    {	
-        "name": "Listen to a podcast",
-        "link": ""
-	},
-    {	
-        "name": "Clean your shoes",
-        "link": "https://www.youtube.com/watch?v=kyGR47nDrus"
-	},
-    {	
-        "name": "Make a new song playlist",
-        "link": ""
-	},
-    {	
-        "name": "Rearrange your furniture",
-        "link": ""
-	},
-    {   	
-        "name": "Watch a TED Talk",
-        "link": "https://www.ted.com/recommends?exploreCTASource=topics-directory.banner"
-	},
-    {	
-        "name": "Cook something with what you already have",
-        "link": "https://www.supercook.com/#/recipes"
-	},
-    {	
-        "name": "Clean out your closet and sell old clothes online",
-        "link": "https://www.thredup.com/cleanout"
-	},
-    {	
-        "name": "Take a free online class",
-        "link": "https://learndigital.withgoogle.com/digitalgarage"
-	},
-    {	
-        "name": "Meditate",
-        "link": ""
-	},
-    {	
-        "name": "Learn how to do calligraphy",
-        "link": "https://www.youtube.com/watch?v=sBoVGqiSzr4"
-	},
-    {	
-        "name": "Learn how to take better photos with your phone",
-        "link": "https://www.youtube.com/watch?v=AywNcZL6IQQ"
-	},
-    {	
-        "name": "Create a personal budget ",
-        "link": ""
-	},
-    {	
-        "name": "Go on a walk",
-        "link": ""
-	},
-    {	
-        "name": "Make a new music playlist",
-        "link": ""
-	},
-    {	
-        "name": "Choose and implement a few sustainable practices into your life",
-        "link": "https://theminimalistvegan.com/live-a-more-sustainable-lifestyle/"
-	},
-    {	
-        "name": "Find a joke and tell a friend",
-        "link": "https://www.boredpanda.com/funniest-two-line-jokes/?utm_source=google&utm_medium=organic&utm_campaign=organic"
+        name: 'Tidy up with Marie Kondo',
+        link: 'https://www.youtube.com/watch?v=TXzcmr2WcDA'
     },
     {
-        "name": "Read a book",
-        "link": "https://www.goodreads.com/choiceawards/best-books-2019"
+        name: 'Take a new LinkedIn profile picture',
+        link: ''
     },
     {
-        "name": "Learn how to make a website",
-        "link": "https://www.w3schools.com/howto/howto_make_a_website.asp"
+        name: 'Update your LinkedIn',
+        link: ''
     },
     {
-        "name": "Learn how to solve the Rubik's cube",
-        "link": "https://ruwix.com/the-rubiks-cube/how-to-solve-the-rubiks-cube-beginners-method/"
+        name: 'Write a letter to a friend',
+        link: ''
     },
     {
-        "name": "Read a random Wikipedia article",
-        "link": "https://en.wikipedia.org/wiki/Special:Random"
+        name: 'Listen to a podcast',
+        link: ''
     },
     {
-        "name": "Do 10 squats",
-        "link": ""
+        name: 'Clean your shoes',
+        link: 'https://www.youtube.com/watch?v=kyGR47nDrus'
     },
     {
-        "name": "Read about the Demon core",
-        "link": "https://en.wikipedia.org/wiki/Demon_core"
+        name: 'Make a new song playlist',
+        link: ''
     },
     {
-        "name": "Learn how to darn a sock",
-        "link": "https://www.theartofdoingstuff.com/how-to-darn-a-sock/"
+        name: 'Rearrange your furniture',
+        link: ''
     },
     {
-        "name": "Check your Facebook privacy settings",
-        "link": "https://www.facebook.com/help/193677450678703/"
+        name: 'Watch a TED Talk',
+        link: 'https://www.ted.com/recommends?exploreCTASource=topics-directory.banner'
     },
     {
-        "name": "Memorize more digits of Pi",
-        "link": "https://www.piday.org/million/"
+        name: 'Cook something with what you already have',
+        link: 'https://www.supercook.com/#/recipes'
     },
     {
-        "name": "Do your laundry",
-        "link": ""
+        name: 'Clean out your closet and sell old clothes online',
+        link: 'https://www.thredup.com/cleanout'
     },
     {
-        "name": "Clean your bathroom",
-        "link": ""
+        name: 'Take a free online class',
+        link: 'https://learndigital.withgoogle.com/digitalgarage'
     },
     {
-        "name": "Test your typing speed",
-        "link": "https://10fastfingers.com/typing-test/english"
+        name: 'Meditate',
+        link: ''
     },
     {
-        "name": "Get a plant",
-        "link": ""
+        name: 'Learn how to do calligraphy',
+        link: 'https://www.youtube.com/watch?v=sBoVGqiSzr4'
     },
     {
-        "name": "Drink some water",
-        "link": ""
+        name: 'Learn how to take better photos with your phone',
+        link: 'https://www.youtube.com/watch?v=AywNcZL6IQQ'
     },
     {
-        "name": "Listen to your favorite song",
-        "link": ""
+        name: 'Create a personal budget ',
+        link: ''
     },
     {
-        "name": "Call your mom",
-        "link": ""
+        name: 'Go on a walk',
+        link: ''
     },
     {
-        "name": "Read about a TV trope",
-        "link": "https://tvtropes.org/"
+        name: 'Make a new music playlist',
+        link: ''
     },
     {
-        "name": "Learn to draw",
-        "link": "https://drawabox.com/"
+        name: 'Choose and implement a few sustainable practices into your life',
+        link: 'https://theminimalistvegan.com/live-a-more-sustainable-lifestyle/'
     },
     {
-        "name": "Change your passwords",
-        "link": ""
+        name: 'Find a joke and tell a friend',
+        link: 'https://www.boredpanda.com/funniest-two-line-jokes/?utm_source=google&utm_medium=organic&utm_campaign=organic'
     },
     {
-        "name": "Cook something you've never cooked before",
-        "link": ""
+        name: 'Read a book',
+        link: 'https://www.goodreads.com/choiceawards/best-books-2019'
     },
     {
-        "name": "Reconnect with a friend",
-        "link": ""
+        name: 'Learn how to make a website',
+        link: 'https://www.w3schools.com/howto/howto_make_a_website.asp'
+    },
+    {
+        name: "Learn how to solve the Rubik's cube",
+        link: 'https://ruwix.com/the-rubiks-cube/how-to-solve-the-rubiks-cube-beginners-method/'
+    },
+    {
+        name: 'Read a random Wikipedia article',
+        link: 'https://en.wikipedia.org/wiki/Special:Random'
+    },
+    {
+        name: 'Do 10 squats',
+        link: ''
+    },
+    {
+        name: 'Read about the Demon core',
+        link: 'https://en.wikipedia.org/wiki/Demon_core'
+    },
+    {
+        name: 'Learn how to darn a sock',
+        link: 'https://www.theartofdoingstuff.com/how-to-darn-a-sock/'
+    },
+    {
+        name: 'Check your Facebook privacy settings',
+        link: 'https://www.facebook.com/help/193677450678703/'
+    },
+    {
+        name: 'Memorize more digits of Pi',
+        link: 'https://www.piday.org/million/'
+    },
+    {
+        name: 'Do your laundry',
+        link: ''
+    },
+    {
+        name: 'Clean your bathroom',
+        link: ''
+    },
+    {
+        name: 'Test your typing speed',
+        link: 'https://10fastfingers.com/typing-test/english'
+    },
+    {
+        name: 'Get a plant',
+        link: ''
+    },
+    {
+        name: 'Drink some water',
+        link: ''
+    },
+    {
+        name: 'Listen to your favorite song',
+        link: ''
+    },
+    {
+        name: 'Call your mom',
+        link: ''
+    },
+    {
+        name: 'Read about a TV trope',
+        link: 'https://tvtropes.org/'
+    },
+    {
+        name: 'Learn to draw',
+        link: 'https://drawabox.com/'
+    },
+    {
+        name: 'Change your passwords',
+        link: ''
+    },
+    {
+        name: "Cook something you've never cooked before",
+        link: ''
+    },
+    {
+        name: 'Reconnect with a friend',
+        link: ''
+    },
+    {
+        name: 'Close at least 5 tabes in your browser',
+        link: ''
+    },
+    {
+        name: 'Learn about alternatives to google search',
+        link: 'https://fossbytes.com/google-alternative-best-search-engine/'
+    },
+    {
+        name: 'Watch Gourmet Pringles attempt',
+        link: 'https://www.youtube.com/watch?v=J3xLG-whfFk'
+    },
+    {
+        name: 'Learn how to make a diamonds carbonated water',
+        link: 'https://youtu.be/n0wvDwSnzcw'
+    },
+    {
+        name: 'Relish a mechanical butterfly',
+        link: 'https://youtu.be/1gu3z7w4Vc8'
+    },
+    {
+        name: 'Think about the fact that brown does not exist',
+        link: 'https://youtu.be/wh4aWZRtTwU'
+    },
+    {
+        name: 'Learn about Snake Island',
+        link: 'https://en.wikipedia.org/wiki/Ilha_da_Queimada_Grande'
+    },
+    {
+        name: 'Crush your friend in Rock Paper Scissors',
+        link: 'https://www.youtube.com/watch?v=rudzYPHuewc'
+    },
+    {
+        name: 'Learn the best skill for your resume',
+        link: 'https://www.reddit.com/r/AskReddit/comments/1sn8ki/what_is_a_skill_someone_can_learn_in_6_months/cdzjm0e?utm_source=share&utm_medium=web2x'
+    },
+    {
+        name: 'Learn a paradox',
+        link: 'https://www.reddit.com/r/AskReddit/comments/4f50wo/whats_the_best_paradox_you_know/'
+    },
+    {
+        name: 'Read a short comics',
+        link: 'https://xkcd.com/'
+    },
+    {
+        name: "Learn about a game Ubisoft won't let you play",
+        link: 'https://www.youtube.com/watch?v=RTkxzQDo0ng'
+    },
+    {
+        name: 'Watch a strange youtube series',
+        link: 'https://www.youtube.com/watch?v=F8nUe8D5-w0&list=PLnN2bBxGARv7HS7cUsAUdfgPhKBnoUIKA'
+    },
+    {
+        name: 'Get a desk plant',
+        link: 'https://www.youtube.com/watch?v=73wGYLPXlXk'
+    },
+    {
+        name: 'Backup your photos',
+        link: ''
+    },
+    {
+        name: 'Clean your desktop',
+        link: ''
+    },
+    {
+        name: 'Clean your home screen',
+        link: ''
+    },
+    {
+        name: 'Count binary on you fingers',
+        link: 'https://www.youtube.com/watch?v=apCLHmPsC68'
+    },
+    {
+        name: 'Have you ever had a dream like this?',
+        link: 'https://youtu.be/dXidW7fEH8g'
+    },
+    {
+        name: 'Revisit an old video in your youtube history',
+        link: ''
+    },
+    {
+        name: 'Do some painting',
+        link: 'https://youtu.be/V6l2py68WY4'
+    },
+    {
+        name: 'Say Hey to a Ron',
+        link: 'https://youtu.be/zBJU9ndpH1Q'
+    },
+    {
+        name: 'Take a guitar lesson',
+        link: 'https://youtu.be/m24DPDKZ4zA'
     }
-];
+]
